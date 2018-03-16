da491c6f3d2db72904df29349d56601a7b299f96
Fredric Vollmer <fredricvollmer@Fredrics-iMac.local>
2018-03-15 22:26:41 -0600

    2018-03-15

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # Build zip
 rm -f build.zip
-zip -r build.zip environment.yml matrixmaster Dockerfile
+zip -r build.zip environment.yml matrixmaster Dockerfile .ebextensions
 
 # Deploy to EB
 eb deploy
